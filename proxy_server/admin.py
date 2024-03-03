b8e05ee3e7c62ccb3d1f920754e24a55e28abbd4
Вадим Акимов <sfc.coltrane@gmail.com>
2024-03-03 10:59:34 +0300

    Added model

--- proxy_server/admin.py
+++ proxy_server/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from proxy_server.models import ProxyServer
+
+
+@admin.register(ProxyServer)
+class ProxyServerAdmin(admin.ModelAdmin):
+    list_display = (
+        'created',
+        'updated',
+        'ip',
+        'port',
+        'proxy_type',
+        'login',
+        'password',
+        'country',
+        'available',
+    )
+    list_filter = ('created', 'updated', 'proxy_type', 'available')
--- proxy_server/models.py
+++ proxy_server/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 
+from proxy_server.base.base_models import TimestampModel, TypeChoices
+
+
+class ProxyServer(TimestampModel):
+    ip = models.CharField(verbose_name='IP-адрес сервера', max_length=64)
+    port = models.PositiveIntegerField(verbose_name='TCP/IP-порт сервера (1025..65635)')
+    proxy_type = models.CharField(choices=TypeChoices.choices, default=TypeChoices.HTTP, max_length=64)
+    login = models.CharField(verbose_name='Логин', max_length=256)
+    password = models.CharField(verbose_name='Пароль', max_length=256)
+    country = models.CharField(verbose_name='Страна', max_length=256)
+    available = models.BooleanField(verbose_name='Доступность')
+
+    class Meta:
+        verbose_name = 'Прокси сервер'
+        verbose_name_plural = 'Прокси серверы'
